fe8f825aed9d196ea362154bb621965308fe2e7f
Jon Canady <jon@joncanady.com>
2011-04-26 23:18:52 -0400

    Formatting change.

--- lib/interfax/incoming.rb
+++ lib/interfax/incoming.rb
@@ -1,21 +1,22 @@
 require 'soap/wsdlDriver'
 
 module Interfax
-
+  
   class Incoming
-
+    
     class << self
       attr_accessor :username, :password, :mark_as_read, :limit
-
+      
       def query(type, opts = {})
-   
-        result = SOAP::WSDLDriverFactory.new("https://ws.interfax.net/inbound.asmx?WSDL").create_rpc_driver.GetList(
-          :Username => self.username,
-          :Password => self.password,
-          :MaxItems => opts[:MaxItems] || self.limit || 100,
-          :MarkAsRead => opts[:MarkAsRead] || self.mark_as_read || false,
-          :LType => type
-        )
+        result = SOAP::WSDLDriverFactory.
+          new("https://ws.interfax.net/inbound.asmx?WSDL").
+          create_rpc_driver.
+          GetList(:Username => self.username,
+                  :Password => self.password,
+                  :MaxItems => opts[:MaxItems] || self.limit || 100,
+                  :MarkAsRead => opts[:MarkAsRead] || self.mark_as_read || false,
+                  :LType => type
+                  )
         return [] if result.nil? || !defined?(result.objMessageItem)
         result.objMessageItem.messageItem
       end
