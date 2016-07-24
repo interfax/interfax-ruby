d7624cdf201256458c7283369ca3acc2ca509392
Cristiano Betta <cbetta@gmail.com>
2016-07-24 14:15:21 +0100

    Remove SOAP client

--- lib/interfax.rb
+++
@@ -1,9 +0,0 @@
-require 'time'
-require "soap/wsdlDriver"
-
-$:.unshift(File.dirname(__FILE__)) unless
-  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
-
-require 'interfax/fax_item'
-require 'interfax/base'
-require 'interfax/incoming'
